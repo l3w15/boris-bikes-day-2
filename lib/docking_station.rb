c3f9efd43e70b792689e1f3285391d427db0a00a
Lewis Watson <lewwwiswatson@gmail.com>
2018-01-24 16:22:30 +0000

    Add bikes array to track docking and releasing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,24 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    Bike.new
+    fail "There are no bikes available" unless @bikes[0]
+    @bikes.pop
+  end
+
+  # def show_bike
+  #   @bikes
+  # end
+
+  def dock(bike)
+    fail "Capacity is full" if @bikes.length >= 20
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,38 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
+
+
   it { is_expected.to respond_to(:release_bike) }
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it 'allows bikes to dock' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to match_array([bike])
+  end
+
+  it { is_expected.to respond_to(:bikes) }
+
+  it 'shows bikes that have been docked' do
+    bike = Bike.new
+    docked_bike = subject.dock(bike)
+    expect(subject.bikes).to eq([bike])
+  end
+
+  it 'doesn\'t release a bike if @bikes is empty' do
+    expect { subject.release_bike }.to raise_error "There are no bikes available"
+  end
+
+  it 'doesn\'t allow you to dock a bike once capacity of 20 is reached' do
+    20.times { subject.dock(Bike.new) }
+    expect { subject.dock(Bike.new) }.to raise_error "Capacity is full"
+  end
+
 end
